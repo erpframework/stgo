9d31c0b0ec6c3971f05a6e61fd7ca9c8fb507dcb
alejandrofichera <alejandrofichera@gmail.com>
2012-09-22 01:57:51 +0000

    Se agrega business a la aplicacion web.

---
+++ business/business/Perfil.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace business
+{
+    public class Perfil
+    {
+        public int Id { get; set; }
+        public String Descripcion { get; set; }
+
+        public Perfil(int id, String descripcion) 
+        {
+            this.Id = id;
+            this.Descripcion = descripcion;
+        }
+    }
+}
---
+++ business/business/Sala.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace business
+{
+    public class Sala
+    {
+        public long Id { get; set; }
+        public String Nombre { get; set; }
+        public Boolean PermiteMultiplo { get; set; }
+        public int Frecuencia { get; set; }
+        public DateTime HoraInicio { get; set; }
+        public DateTime HoraCierre { get; set; }
+
+        public Sala(String nombre, Boolean permiteMultiplo, int frecuencia, DateTime horaInicio, DateTime horaCierre) 
+        {
+            this.Nombre = nombre;
+            this.PermiteMultiplo = permiteMultiplo;
+            this.Frecuencia = frecuencia;
+            this.HoraInicio = horaInicio;
+            this.HoraCierre = horaCierre;
+        }
+    }
+}
